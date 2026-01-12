9bf918cf78f8d7bb83bbc75da74e5b01acaf1efa
Piotr Haber <hpiotrek080@gmail.com>
2026-01-13 00:06:14 +0100

    rejected the rust idea

--- src/flight_controller/src/main.rs
+++
@@ -1,8 +0,0 @@
-use r2r;
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let ctx = r2r::Context::create()?;
-    let mut node = r2r::Node::create(ctx, "test_node", "");
-
-}
